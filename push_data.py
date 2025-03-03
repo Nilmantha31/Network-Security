ef4fbe77cd5b9a66a5b6658c5dc05104aa74ab34
Nilmantha31 <sanjunilmantha16@gmail.com>
2025-03-03 21:35:00 +0530

    completed data config

--- push_data.py
+++ push_data.py
@@ -22,7 +22,7 @@ class NetworkDataExtract:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-    def cv_to_json_convertor(self,file_path):
+    def csv_to_json_convertor(self,file_path):
         try:
             data = pd.read_csv(file_path)
             data.reset_index(drop=True, inplace=True)
@@ -48,12 +48,12 @@ class NetworkDataExtract:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
-if __name__ == '__main__':
-    file_path = 'Network_Data\phisingData.csv'
-    database = 'NilmanthaAI'
-    collection = 'NetworkData'
-    networkobj = NetworkDataExtract()
-    records = networkobj.cv_to_json_convertor(file_path=file_path)
-    print(records)
-    no_of_records = networkobj.inser_data_to_mongodb(records=records,database=database,collection=collection)
-    print(no_of_records)
+# if __name__ == '__main__':
+#     file_path = 'Network_Data\phisingData.csv'
+#     database = 'NilmanthaAI'
+#     collection = 'NetworkData'
+#     networkobj = NetworkDataExtract()
+#     records = networkobj.csv_to_json_convertor(file_path=file_path)
+#     print(records)
+#     no_of_records = networkobj.inser_data_to_mongodb(records=records,database=database,collection=collection)
+#     print(no_of_records)
